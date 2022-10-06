6dfc18719d5c1681c002d3fb0ab2e621a49c2d7c
Emilio Resende <emilio.murta@msitbrasil.com>
2022-10-05 21:55:22 -0300

    finalize header

--- components/shared/Header.component.tsx
+++ components/shared/Header.component.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, Text } from "react-native";
+import { Dimensions, Image, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HeaderBackButton } from "@react-navigation/elements";
+
+import logo from "../../assets/icon.png";
+import { Platform } from "react-native";
 
 export default function HeaderComponent(props: any) {
     const { canReturn } = props;
@@ -8,6 +11,7 @@ export default function HeaderComponent(props: any) {
     return (<SafeAreaView style={styles.container}>
         { canReturn ? <HeaderBackButton onPress={() => props.navigation.goBack()} /> : null }
         <Text style={styles.title}>Calculadora IMC</Text>
+        <Image source={logo} style={styles.logo} />
     </SafeAreaView>);
 }
 
@@ -17,13 +21,36 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignContent: 'center',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        shadowColor: '#aaa',
+        shadowOpacity: 0.8,
+        shadowRadius: 1,
+        shadowOffset: { 
+            height: 1,
+            width: 1
+        },
+        elevation: 10,
+        paddingBottom: Platform.OS === 'ios'
+            ? Dimensions.get('screen').width * 0.0
+            : Dimensions.get('screen').width * 0.1,
+        paddingTop: Platform.OS === 'ios'
+        ? Dimensions.get('screen').width * 0.07
+        : Dimensions.get('screen').width * 0.05,
+
     },
     title: {
-        flex: 5,
+        flex: 4,
         color: '#000',
-        marginLeft: 10,
-        fontSize: 14,
-        height: 20
+        backgroundColor: '#fff',
+        marginLeft: Dimensions.get('screen').width * 0.05,
+        height: Dimensions.get('screen').height * 0.06,
+        fontSize: Dimensions.get('window').width * 0.07,
+    },
+    logo: {
+        flex: 1,
+        resizeMode: 'contain',
+        height: Dimensions.get('screen').height * 0.06,
+        width: Dimensions.get('screen').height * 0.06,
     }
 });
